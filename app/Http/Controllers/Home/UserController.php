28454ac3243781c4546eb4322578650d9ae12456
dongfangxiaoshuo <1563930710@qq.com>
2019-07-17 16:37:06 +0800

    7.17 维护了user

--- app/Http/Controllers/Home/UserController.php
+++ app/Http/Controllers/Home/UserController.php
@@ -9,14 +9,32 @@ use App\User;
 class UserController extends Controller
 {
     public function to_login(Request $request){
-        
-        $phone = $request->phone;
-        // return $phone;
-        $sms = $request->sms;
-        $user = User::firstOrCreate(['phone' => 18600611497]);
-        return $user;
 
-        $username = $request->username;
-        $possword = $request->possword;
+        if(isset($request->phone)) {
+            $phone = $request->phone;
+            $sms = $request->sms;
+            //短信验证成功后 执行
+            $user = User::firstOrCreate(['phone' =>  $phone]);
+            
+        } 
+        // return $request;
+        if(isset($request->username)){
+            $username = $request->username;  
+            $password = $request->password;
+            $user = User::where('username',$username)->get();
+            if($user!=null){
+                $user = User::where('id',$user->id)->where('password',$password)->get();
+            }else{
+                $user = new User;
+                $user->username = $username;
+                $user->password = encrypt($password);
+                $user->token = $username;
+        
+                $flight->save();
+            }
+           
+        
+        }  
+        return $user;
     }
 }
